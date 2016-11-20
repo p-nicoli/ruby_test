fad675c4c0665a78a90239faf1884fff13a4e1cf
pnicoli <pierre.nicoli@gmail.com>
2016-11-20 17:48:25 +0100

    Some cleaning up and refactoring.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,38 +1,42 @@
 class Event < ActiveRecord::Base
+  validates_inclusion_of :kind, :in => %w(opening appointment)
 
-  validates_inclusion_of :kind, :in => ['opening', 'appointment']
+  public
+  RANGE_OF_SEARCH = 6.days
 
-  TIME = "%k:%M"
-  DATE = "%Y/%m/%d"
-  OPENING_DURATION = 30.minutes
-  MAX_LENGTH = 7
+  def self.availabilities(from_date=DateTime.now)
+    request_interval = from_date..from_date+RANGE_OF_SEARCH
 
-  def self.availabilities(from_date=DateTime.parse("1970-01-01"))
-    request_interval = from_date..from_date+6.days
-
-    openings = Event.where(kind: "opening", starts_at: request_interval) + recurring_openings(from_date)
-    appointments = Event.where(kind: "appointment", starts_at: request_interval)
+    openings = Event.where(kind: 'opening', starts_at: request_interval) + recurring_openings(from_date)
+    appointments = Event.where(kind: 'appointment', starts_at: request_interval)
 
     (request_interval).collect do |day|
-      availability = Hash.new
-      availability[:date] = day
-      availability[:slots] = (make_slots(day, openings)-make_slots(day, appointments)).sort {|x,y| Time.parse(x) <=> Time.parse(y) }
-      availability
+      compose_availability(day, openings, appointments)
     end
-
   end
 
-  def self.make_slots(day, openings)
-    openings.select { |opening| opening.starts_at.to_date == day }.collect do |opening|
-      get_slots(opening.starts_at, opening.ends_at)
-    end.flatten.uniq
+  private
+  TIME = '%k:%M'
+  OPENING_DURATION = 30.minutes
+
+  def self.compose_availability(day, openings, appointments)
+    {
+        date: day,
+        slots: get_available_slots(day, openings, appointments)
+    }
+  end
+
+  def self.get_available_slots(day, openings, appointments)
+    (make_slots_from_events(openings, day)-make_slots_from_events(appointments, day)).sort! do |x, y|
+      Time.parse(x) <=> Time.parse(y)
+    end
   end
 
   def self.recurring_openings(from_date)
-    recurring_openings = Event.where("kind = ? and weekly_recurring = ?", 'opening', true)
+    recurring_openings = Event.where('kind = ? and weekly_recurring = ?', 'opening', true)
 
     repeated_openings = []
-    (from_date..from_date+6.days).each do |day|
+    (from_date..from_date+RANGE_OF_SEARCH).each do |day|
       same_day_openings = recurring_openings.select { |opening| opening.starts_at.wday == day.wday}
       same_day_openings.each do |opening|
         new_starts_at = opening.starts_at + (day - opening.starts_at.to_date).days
@@ -42,6 +46,12 @@ class Event < ActiveRecord::Base
       end
     end
     repeated_openings
+  end
+
+  def self.make_slots_from_events(events, day)
+    events.select { |event| event.starts_at.to_date == day }.collect do |opening|
+      get_slots(opening.starts_at, opening.ends_at)
+    end.flatten.uniq
   end
 
   def self.get_slots(start_datetime, end_datetime)
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
 
-  test "Event should create stuff" do
+  test 'Event should create stuff' do
     #Given
     Event.create kind: 'opening'
 
@@ -14,21 +14,7 @@ class EventTest < ActiveSupport::TestCase
     assert_equal 1, found_events.size
   end
 
-  # Works when using Enum for openings but makes the database field change to int type
-  #test "Event should only create openings and appointments" do
-  #  #Given
-  #
-  #  #When
-  #  Event.create kind: 'opening'
-  #  Event.create kind: 'appointment'
-  #
-  #  #Then
-  #  assert_raise ArgumentError do
-  #    Event.create kind: 'lunch-break'
-  #  end
-  #end
-
-  test "availabilities should return an array of 7 elements" do
+  test 'availabilities should return an array of 7 elements' do
     #When
     availabilities = Event.availabilities
 
@@ -36,137 +22,138 @@ class EventTest < ActiveSupport::TestCase
     assert_equal 7, availabilities.length
   end
 
-  test("availabilities should return an array of hashes where keys correspond to week following request") do
+  test 'availabilities should return an array of hashes where keys correspond to week following request' do
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-05-23")
+    availabilities = Event.availabilities DateTime.parse('2016-05-23')
 
     #Then
-    assert_equal Date.parse("2016-05-23"), availabilities[0][:date]
-    assert_equal Date.parse("2016-05-24"), availabilities[1][:date]
-    assert_equal Date.parse("2016-05-25"), availabilities[2][:date]
-    assert_equal Date.parse("2016-05-26"), availabilities[3][:date]
-    assert_equal Date.parse("2016-05-27"), availabilities[4][:date]
-    assert_equal Date.parse("2016-05-28"), availabilities[5][:date]
-    assert_equal Date.parse("2016-05-29"), availabilities[6][:date]
+    assert_equal Date.parse('2016-05-23'), availabilities[0][:date]
+    assert_equal Date.parse('2016-05-24'), availabilities[1][:date]
+    assert_equal Date.parse('2016-05-25'), availabilities[2][:date]
+    assert_equal Date.parse('2016-05-26'), availabilities[3][:date]
+    assert_equal Date.parse('2016-05-27'), availabilities[4][:date]
+    assert_equal Date.parse('2016-05-28'), availabilities[5][:date]
+    assert_equal Date.parse('2016-05-29'), availabilities[6][:date]
   end
 
-  test("availabilities should return slots of 30 minutes openings") do
+  test 'availabilities should return slots of 30 minutes openings' do
     #Given
-    create_event('opening', "2016-05-23 9:30", "2016-05-23 10:30", false)
+    create_event('opening', '2016-05-23 9:30', '2016-05-23 10:30', false)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-05-20")
+    availabilities = Event.availabilities DateTime.parse('2016-05-20')
 
     #Then
-    assert_equal ["9:30","10:00"], availabilities[3][:slots]
+    assert_equal %w(9:30 10:00), availabilities[3][:slots]
   end
 
-  test("availabilities should return slots of 30 minutes openings over the next 7 days of request") do
+  test 'availabilities should return slots of 30 minutes openings over the next 7 days of request' do
     #Given
-    create_event('opening', "2016-04-01 9:30", "2016-05-23 10:30", false)
-    create_event('opening', "2016-05-23 9:30", "2016-05-23 10:30", false)
-    create_event('opening', "2016-05-25 9:00", "2016-05-25 10:30", false)
-    create_event('opening', "2016-05-28 9:30", "2016-05-28 10:30", false)
+    create_event('opening', '2016-04-01 9:30', '2016-05-23 10:30', false)
+    create_event('opening', '2016-05-23 9:30', '2016-05-23 10:30', false)
+    create_event('opening', '2016-05-25 9:00', '2016-05-25 10:30', false)
+    create_event('opening', '2016-05-28 9:30', '2016-05-28 10:30', false)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-05-20")
+    availabilities = Event.availabilities DateTime.parse('2016-05-20')
 
     #Then
-    assert_equal ["9:30","10:00"], availabilities[3][:slots]
-    assert_equal ["9:00","9:30","10:00"], availabilities[5][:slots]
+    assert_equal %w(9:30 10:00), availabilities[3][:slots]
+    assert_equal %w(9:00 9:30 10:00), availabilities[5][:slots]
   end
 
-  test("availabilities should return weekly recurring openings") do
+  test 'availabilities should return weekly recurring openings' do
     #Given
-    create_event("opening", "2016-04-01 09:30", "2016-04-01 12:00", true)
+    create_event('opening', '2016-04-01 09:30', '2016-04-01 12:00', true)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-05-20")
+    availabilities = Event.availabilities DateTime.parse('2016-05-20')
 
     #Then
     (1..6).each { |i| assert_equal [], availabilities[i][:slots] }
-    assert_equal ["9:30","10:00","10:30","11:00","11:30"], availabilities[0][:slots]
+    assert_equal %w(9:30 10:00 10:30 11:00 11:30), availabilities[0][:slots]
   end
 
-  test("availabilities should exclude appointments") do
+  test 'availabilities should exclude appointments' do
     #Given
-    create_event('opening', "2016-04-01 10:00", "2016-04-01 12:00", false)
-    create_event('appointment', "2016-04-01 10:30", "2016-04-01 11:00", false)
+    create_event('opening', '2016-04-01 10:00', '2016-04-01 12:00', false)
+    create_event('appointment', '2016-04-01 10:30', '2016-04-01 11:00', false)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-04-01")
+    availabilities = Event.availabilities DateTime.parse('2016-04-01')
 
     #Then
-    assert_equal ["10:00","11:00","11:30"], availabilities[0][:slots]
+    assert_equal %w(10:00 11:00 11:30), availabilities[0][:slots]
   end
 
-  test "availabilities should not return duplicate slots" do
+  test 'availabilities should not return duplicate slots' do
     #Given
-    create_event('opening', "2016-02-28 12:00", "2016-02-28 13:30", true)
-    create_event('opening', "2016-02-28 12:00", "2016-02-28 13:30", false)
+    create_event('opening', '2016-02-28 12:00', '2016-02-28 13:30', true)
+    create_event('opening', '2016-02-28 12:00', '2016-02-28 13:30', false)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-02-28")
-
-    #Then
-    assert_equal Date.new(2016, 2, 28), availabilities[0][:date]
-    assert_equal ["12:00", "12:30", "13:00"], availabilities[0][:slots]
-  end
-
-  test "availabilities should never ignore appointments even if openings are made after making appointments" do
-    #Given
-    create_event('opening', "2016-02-28 10:00", "2016-02-28 14:00", true)
-    create_event('appointment', "2016-02-28 12:00", "2016-02-28 13:30", false)
-    create_event('opening', "2016-02-28 12:00", "2016-02-28 13:30", false)
-
-    #When
-    availabilities = Event.availabilities DateTime.parse("2016-02-28")
+    availabilities = Event.availabilities DateTime.parse('2016-02-28')
 
     #Then
     assert_equal Date.new(2016, 2, 28), availabilities[0][:date]
-    assert_equal ["10:00", "10:30", "11:00", "11:30", "13:30"], availabilities[0][:slots]
+    assert_equal %w(12:00 12:30 13:00), availabilities[0][:slots]
   end
 
-  test "availabilities should correcly return an empty array of slots if all openings are taken by appointments" do
+  test 'availabilities should never ignore appointments even if openings are made after making appointments' do
     #Given
-    create_event('opening', "2016-02-28 10:00", "2016-02-28 14:00", true)
-    create_event('appointment', "2016-02-28 10:00", "2016-02-28 14:30", false)
+    create_event('opening', '2016-02-28 10:00', '2016-02-28 14:00', true)
+    create_event('appointment', '2016-02-28 12:00', '2016-02-28 13:30', false)
+    create_event('opening', '2016-02-28 12:00', '2016-02-28 13:30', false)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-02-28")
+    availabilities = Event.availabilities DateTime.parse('2016-02-28')
+
+    #Then
+    assert_equal Date.new(2016, 2, 28), availabilities[0][:date]
+    assert_equal %w(10:00 10:30 11:00 11:30 13:30), availabilities[0][:slots]
+  end
+
+  test 'availabilities should correctly return an empty array of slots if all openings are taken by appointments' do
+    #Given
+    create_event('opening', '2016-02-28 10:00', '2016-02-28 14:00', true)
+    create_event('appointment', '2016-02-28 10:00', '2016-02-28 14:30', false)
+
+    #When
+    availabilities = Event.availabilities DateTime.parse('2016-02-28')
 
     #Then
     assert_equal Date.new(2016, 2, 28), availabilities[0][:date]
     assert_equal [], availabilities[0][:slots]
   end
 
-  test "availabilities should correctly merge openings" do
+  test 'availabilities should correctly merge openings' do
     #Given
-    create_event('opening', "2016-05-10 10:00", "2016-05-10 12:00", false)
-    create_event('opening', "2016-05-10 11:30", "2016-05-10 13:00", false)
+    create_event('opening', '2016-05-10 10:00', '2016-05-10 12:00', false)
+    create_event('opening', '2016-05-10 11:30', '2016-05-10 13:00', false)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-05-10")
+    availabilities = Event.availabilities DateTime.parse('2016-05-10')
 
     #Then
     assert_equal Date.new(2016, 5, 10), availabilities[0][:date]
-    assert_equal ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30"], availabilities[0][:slots]
+    assert_equal %w(10:00 10:30 11:00 11:30 12:00 12:30), availabilities[0][:slots]
   end
 
-  test "availabilities should correctly merge recurring openings and keep them ordered" do
+  test 'availabilities should correctly merge recurring openings and keep them ordered' do
     #Given
-    create_event('opening', "2016-05-10 9:00", "2016-05-10 12:00", true)
-    create_event('opening', "2016-05-10 11:30", "2016-05-10 12:30", true)
-    create_event('opening', "2016-05-24 11:30", "2016-05-24 13:00", false)
+    create_event('opening', '2016-05-10 09:00', '2016-05-10 12:00', true)
+    create_event('opening', '2016-05-10 11:30', '2016-05-10 12:30', true)
+    create_event('opening', '2016-05-24 11:30', '2016-05-24 13:00', false)
 
     #When
-    availabilities = Event.availabilities DateTime.parse("2016-05-24")
+    availabilities = Event.availabilities DateTime.parse('2016-05-24')
 
     #Then
     assert_equal Date.new(2016, 5, 24), availabilities[0][:date]
-    assert_equal ["9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30"], availabilities[0][:slots]
+    assert_equal %w(9:00 9:30 10:00 10:30 11:00 11:30 12:00 12:30), availabilities[0][:slots]
   end
 
+  #Leaving this one verbatim
   test "one simple test example" do
 
     Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
